98a60e2c15df30d83d4aaad0865f1e8c234e383f
techpology <alirashini2323@outlook.com>
2024-09-01 16:17:23 +0200

    Testing unit test implementation through pipeline

---
+++ Agent.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Agent.Misc;
+
+namespace Agent.Tests;
+
+[Trait("Category", "Unit")]
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.Equal(3, CI_Pipeline_Test.Int_Ret_Test(3));
+    }
+}
