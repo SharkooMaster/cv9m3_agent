93abe2b55dcd7f21eae11d54ddaa7018b0d3043f
techpology <alirashini2323@outlook.com>
2024-10-05 19:08:10 +0200

    Internal communications init

---
+++ src/Services/Grpc/Internal/QueryAgentService.cs
@@ -0,0 +1,18 @@
+using Grpc.Core;
+using TpInternalService;
+
+namespace Agent.Services.Grpc;
+
+public class QueryAgentService : QueryAgent.QueryAgentBase
+{
+    private readonly ILogger<QueryAgentService> _logger;
+    public QueryAgentService(ILogger<QueryAgentService> logger)
+    {
+        _logger = logger;
+    }
+
+    public override Task<QueryResponse> Query(QueryRequest request, ServerCallContext context)
+    {
+        return base.Query(request, context);
+    }
+}
