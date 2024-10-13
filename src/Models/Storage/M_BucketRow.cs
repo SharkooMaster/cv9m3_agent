a77c3313b01946cdb483c0dc47243a1ae1d3b227
techpology <alirashini2323@outlook.com>
2024-10-13 23:12:15 +0200

    added bucket search. untested

--- src/Models/Storage/M_BucketRow.cs
+++ src/Models/Storage/M_BucketRow.cs
@@ -4,6 +4,6 @@ namespace Agent.Models.Storage;
 public class BucketRow
 {
     public required long id { get; set; }
-    public required double[] vector { get; set; }
+    public required float[] vector { get; set; }
     public required byte[] chunk { get; set; }
 }
--- src/Modules/Storage/BucketManager.cs
+++ src/Modules/Storage/BucketManager.cs
@@ -1,5 +1,21 @@
+using System.Collections.Concurrent;
+using Agent.Models.Storage;
+using TpInternalService;
 namespace Agent.Modules.Storage;
 
 public static class BucketManager
 {
+    public static ConcurrentDictionary<string, Bucket> buckets = new ConcurrentDictionary<string, Bucket>();
+
+    public static async Task<List<TpInternalService.Result>> Search(QueryRequest request)
+    {
+        List<TpInternalService.Result> results = new List<TpInternalService.Result>();
+
+        if(buckets.ContainsKey(request.Key))
+        {
+            results.AddRange(await buckets[request.Key].Search(request.Vector.Vec.ToArray(), request.TopK, request.MinThresh));
+        }
+
+        return results;
+    }
 }
