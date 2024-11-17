06345841ef7bb0420f54b72836915b3b830c29b3
techpology <alirashini2323@outlook.com>
2024-11-17 23:32:04 +0100

    update

--- src/Utilities/Globals.cs
+++ src/Utilities/Globals.cs
@@ -13,6 +13,7 @@ public static class Globals
 
     public static string AgentsLoadbalancer = "http://agent-loadbalancer.default.svc.cluster.local:5000";
 
-    public static string[] KEYS = new string[2] { "" , "" };
+    public static string KEY_MIN = "00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
+    public static string KEY_MAX = "11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111";
     public static Vector2 KEY_RANGE = new Vector2(0, 10);
 }
