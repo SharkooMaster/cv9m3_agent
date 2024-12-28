47057c90fc3967bf941164d1ec7e533ce8b841bf
techpology <alirashini2323@outloo.com>
2024-12-28 13:21:46 +0100

    restarting

--- src/Models/M_VectorBucket.cs
+++
@@ -1,9 +0,0 @@
-
-namespace Agent.Models;
-
-public class M_VectorBucket
-{
-    public string signature { get; set; }
-    public Dictionary<string, float[]> vectors = new Dictionary<string, float[]>();
-    public Dictionary<string, object> metadata = new Dictionary<string, object>();
-}
