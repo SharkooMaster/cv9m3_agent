eeaaad7eb209b30248d2e9c1162201100cbd286a
techpology <alirashini2323@outloo.com>
2024-12-29 18:07:56 +0100

    update

--- src/Utilities/NodeUtils.cs
+++ src/Utilities/NodeUtils.cs
@@ -2,10 +2,19 @@
 using System.Numerics;
 using System.Security.Cryptography;
 using System.Text;
+using Agent.Utils.Misc;
 using Google.Protobuf.Reflection;
 
 namespace Agent.Utils;
 
 public static class NodeUtils
 {
+        public static ulong generateNodeID()
+        {
+            string ip = Misc.Misc.GetLocalIPAddress();
+            string port = "5000";
+            // hash using sha256
+            // use a hilbert curve or z curve to convert from 2d to 1d
+            return 0;
+        }
 }
