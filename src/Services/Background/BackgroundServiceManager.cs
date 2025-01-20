11427a5e8b713d1a641be96bb5a59f553058c0c5
techpology <alirashini2323@outloo.com>
2025-01-20 19:59:15 +0100

    Routines

--- src/Services/Background/BackgroundServiceManager.cs
+++ src/Services/Background/BackgroundServiceManager.cs
@@ -1,12 +1,17 @@
 
+using System.Threading.Tasks;
 using Agent.Modules.Agneta;
 
 public static class BackgrounfServiceManager
 {
     public static Dictionary<string ,Action> RoutineMethods = new Dictionary<string, Action>();
 
-    public static void RegisterRoutineMethod(string _name, Action _func)
+    public static async Task RegisterRoutineMethod(string _name, Action _func)
     {
+        if(!RoutineMethods.TryAdd(_name, _func))
+        {
+            await AgnetaHandler.Log(2, $"Failed to register routine: {_name}");
+        }
     }
 
     public static async void RunRoutineMethods()
@@ -20,8 +25,9 @@ public static class BackgrounfServiceManager
             }
             catch (System.Exception)
             {
-                await AgnetaHandler.Log(2, "")
+                await AgnetaHandler.Log(2, $"Failed to run routine {RoutineMethodsKeys[i]}");
             }
         }
     }
+
 }
