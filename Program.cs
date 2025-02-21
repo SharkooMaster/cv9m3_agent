633e18cd08563a8f9e91e3c3e11de75d4aeaecf7
sharku <alirashini2323@outlook.com>
2025-02-21 13:57:57 +0100

    update

--- Program.cs
+++ Program.cs
@@ -193,5 +193,5 @@ void ConfigureServices(IServiceCollection services)
 {
     services.AddSingleton<AgnetaClientService>(new AgnetaClientService("wss://agneta-loadbalancer.default.svc.cluster.local/log/ws"));
     services.AddSingleton<PushoverClientService>(new PushoverClientService());
-    services.AddSingleton<NetworkFileStorageService>(new NetworkFileStorageService("./data"));
+    services.AddSingleton<NetworkFileStorageService>(new NetworkFileStorageService("/var/ngs/general/data"));
 }
